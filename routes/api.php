87bc75c0aa415f287c3480d511a4b4f28908be93
UnknownFruit <kurianovip@gmail.com>
2026-07-29 20:40:50 +0400

    feat(polls): add PollsController
    - add Api\PollsController with index endpoint
    - register GET api/polls route

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ThemesController;
+use App\Http\Controllers\Api\PollsController;
 
 Route::get('themes', [ThemesController::class, 'index']);
+Route::get('polls', [PollsController::class, 'index']);
